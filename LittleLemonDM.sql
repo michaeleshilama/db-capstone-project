0bb3e656c016a7ada0f509f13b0717a44a353a3f
michaeleshilama <michaeleshilama@cmail.carleton.ca>
2023-09-08 22:12:04 -0400

    Added the stored procedures...this was hard so remember this save

--- LittleLemonDM.sql
+++ LittleLemonDM.sql
@@ -1,31 +1,20 @@
-create view OrdersView as
-Select OrderID, Quantity, TotalCost from Orders;
+drop procedure if exists AddValidBooking;
+Delimiter //
+Create procedure AddvalidBooking(in bDate Date, in tNumber int, in cID int)
+Begin 
+declare message varchar(100);
+declare checker int;
+/*check to see if the date and table number exist already*/
+select count(*) into checker from bookings where Date = bDate and TableNumber=tNumber;
 
-create view OrdersAbove150 as 
-select customers.CustomerID as CustomerID, customers.Name as FullName, orders.OrderID as OrderID, orders.TotalCost as Cost,
-menu.CuisineName as CourseName
-from bookings inner join orders on orders.OrderID = bookings.BookingID
-inner join menu on orders.MenuItem = menu.MenuID
-inner join customers on customers.CustomerID = bookings.CustomerID
-where orders.TotalCost > 150;
+if checker>0 then set message = concat("Cannot book table ",tNumber," on ",bDate," - Booking cancelled");
+else 
+start transaction;
+insert into bookings (BookingID,Date,TableNumber,CustomerID) values(Null,bDate,tNumber,cID); commit;
+set message = concat("Table number ",tNumber," has been booked for customer ",cID," on ",bDate); end if;
 
+select message as "Booking Status"
 
-create view morethan2orders as
-Select CuisineName from menu where MenuID = any(Select MenuItem from orders Group by MenuItem having count(*)>2);
-
-
-create procedure GetMaxQuantity() 
-select Max(TotalQuantity) as 'Max Qunatity in Order' from orders;
-
-Prepare GetOrderDetail from 
-'select orders.OrderID as OrderID, orders.Quantity as Quantity, orders.TotalCost as Cost 
-from customers inner join bookings on customers.customerID = bookings.customerID
-inner join orders on orders.BookingID = bookings.BookingID
-where customers.CustomerID = ?
-';
-
-set @id = 2;
-execute GetOrderDetail using @id;
-
-Drop procedure CancelOrder;
-
+;End //
+Delimiter ;
+call AddValidBooking("2022-10-13",3,4)
